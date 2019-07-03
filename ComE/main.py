4063b323022e79108dd71cd5ff13c2c9b31a950b
Anton Begehr <a.begehr@gmail.com>
2019-07-03 01:56:26 +0100

    set ks to 7 and run again

--- ComE/main.py
+++ ComE/main.py
@@ -50,7 +50,7 @@ if __name__ == "__main__":
     alpha_betas = [(0.1, 5)]
     down_sampling = 0.0
 
-    ks = [2]                                # number of communities
+    ks = [7]                                # number of communities
     walks_filebase = os.path.join('data', input_file)            # where read/write the sampled path
 
 
@@ -151,3 +151,4 @@ if __name__ == "__main__":
                                                                                                                        iter_node,
                                                                                                                             model.k,
                                                                                                                             down_sampling))
+
